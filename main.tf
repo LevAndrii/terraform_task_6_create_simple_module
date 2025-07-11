6c482b07d53b1428b7872fecc4639d051eb226b3
Andrii Levchenko <levchenkoandrii11@gmail.com>
2025-07-11 18:54:04 +0300

    fmt

--- main.tf
+++ main.tf
@@ -16,7 +16,6 @@ provider "azurerm" {
   subscription_id = "4a12acc1-35fb-455f-84dd-d790daaf8781"
 }
 
-
 module "resource_group_storage" {
   source               = "app.terraform.io/levandrii_study/resource_group_storage/azurerm"
   version              = "1.0.7"
@@ -24,4 +23,3 @@ module "resource_group_storage" {
   rg_location          = "West Europe"
   storage_account_name = "tfmoduletestsa2025"
 }
-
